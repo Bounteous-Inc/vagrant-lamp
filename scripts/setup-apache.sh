b6321d7b2ed92320c45b0bbd1ae96a690a8748a8
Michael Kreitzer <mkreitzer@demacmedia.com>
2016-08-17 19:26:33 -0400

    Update to run apache as 'vagrant' user and group to avoid permission issues

--- scripts/setup-apache.sh
+++ scripts/setup-apache.sh
@@ -13,6 +13,9 @@ a2enmod proxy_http
 #Change Listen Port
 sed -i.bak 's/Listen 80$/Listen 8090/' /etc/apache2/ports.conf
 
+#Change user and groups to vagrant
+sed -i.bak 's/www-data$/vagrant/' /etc/apache2/envvars
+
 # Setup VHOST Script
 yes | cp -rf /vagrant/files/vhost.sh /usr/local/bin/vhost
 chmod +x /usr/local/bin/vhost
