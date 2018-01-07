6ebb5e59e6360cbc640d2a198ba77143e4c51253
Martin Francis <martin@classaxe.com>
2018-01-07 07:10:33 -0500

    1.0.2: Bug fix for vstatus

--- files/00-aliases.sh
+++ files/00-aliases.sh
@@ -18,8 +18,8 @@ function vstatus {
   echo -e "\n\033[1;33mvstatus - Vagrant Status\033[0;33m"
   echo -e "  Disk Used:      `df -h --output='pcent' / | tail -n1` (Vagrant) `df -h --output='pcent' /vagrant | tail -n1` (Host)"
   echo -e "  `free | awk '/Mem/{printf(\"Memory used:     %.0f% (RAM)\"), $3/$2*100} /buffers\/cache/{printf(\"      %.0f% (Buffers)\"), 100-($4/($3+$4)*100)}'`"
-  echo -e "  Mysql Status:    $(if [[ $(sudo service mysql   status | grep 'running') != '' ]]; then echo '\033[1;32mOK\033[0m'; else echo '\033[1;31mStopped\033[0;33m'; fi)"
-  echo -e "  Apache2 Status:  $(if [[ $(sudo service apache2 status | grep 'running') != '' ]]; then echo '\033[1;32mOK\033[0m'; else echo '\033[1;31mStopped\033[0;33m'; fi)"
+  echo -e "  Mysql Status:    $(if [[ $(sudo service mysql   status | grep 'is stopped')  == '' ]]; then echo '\033[1;32mOK\033[0;33m'; else echo '\033[1;31mStopped\033[0;33m'; fi)"
+  echo -e "  Apache2 Status:  $(if [[ $(sudo service apache2 status | grep 'not running') == '' ]]; then echo '\033[1;32mOK\033[0;33m'; else echo '\033[1;31mStopped\033[0;33m'; fi)"
   echo -e "\033[0m"
 }
 
